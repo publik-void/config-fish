9746ec7b23e60a00357e0723afd6d28732bb51fb
lasse <lasse-schloer@servermx.de>
2018-05-22 23:41:28 +0200

    improved file description

--- functions/l.fish
+++ functions/l.fish
@@ -1,4 +1,4 @@
-# This file was created by me (incredigital) and inspired by /usr/local/Cellar/fish/2.7.1/share/fish/functions/la.fish
+# This file was created by me (incredigital) and inspired by Homebrew's /usr/local/Cellar/fish/2.7.1/share/fish/functions/la.fish
 
 function l --description "Custom ls variant by me (incredigital)"
     #ls -lhoAF $argv
