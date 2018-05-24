6bacecac1ad4311fb07efb70dda10fae38c4ec4c
lasse <lasse-schloer@servermx.de>
2018-05-24 05:49:23 +0200

    removed -o option on FreeBSD

--- functions/l.fish
+++ functions/l.fish
@@ -1,7 +1,12 @@
 # This file was created by me (incredigital) and inspired by Homebrew's /usr/local/Cellar/fish/2.7.1/share/fish/functions/la.fish
 
 function l --description "Custom ls variant by me (incredigital)"
-    #ls -lhoAF $argv
-    ls -lhoA $argv
+  set opts -l -h -A
+  switch (uname)
+    case FreeBSD
+    case '*'
+      set opts $opts -o 
+  end
+  ls $opts $argv
 end
 
