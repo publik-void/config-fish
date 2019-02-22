c5e307e9baa498db5ee5d27341298470754d0142
lasse <lasse-schloer@servermx.de>
2019-02-23 00:56:31 +0100

    added automatically updated fish completions, just as a comment for now though

--- config.fish
+++ config.fish
@@ -1,7 +1,12 @@
-# This file was created by me (incredigital)
 switch (hostname)
 case lasse-mbp-0
   export LANG=en_US
 end
 
 set -Ux EDITOR nvim
+
+## This is suggested by the kitty docs as a way to keep fish completions up-to-
+## date. But it adds a substantial amount of startup time to the fish shell.
+#if type -q kitty
+#  kitty + complete setup fish | source
+#end
