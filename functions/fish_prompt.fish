d2a88972f05d274a0e9d6da50dd59f8ca7b6476a
lasse <lasse-schloer@servermx.de>
2019-03-05 09:42:25 +0100

    changed prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,93 +1,35 @@
 function fish_prompt --description 'Write out the prompt'
-	set -l last_status $status
+  set -l last_status $status
+  # I specify colors directly here. I could alternatively use fish's universal
+  # variables like fish_color_error, defined in ~/.config/fish/fish_variables
+  set -l prompt_color green
+  if not test $last_status -eq 0
+    set prompt_color brred
+  end
 
-    if not set -q __fish_git_prompt_show_informative_status
-        set -g __fish_git_prompt_show_informative_status 1
+  set -l prompt_text
+  switch "$USER"
+    case root toor
+      set prompt_text '# '
+    case '*'
+      set prompt_text '> '
     end
-    if not set -q __fish_git_prompt_hide_untrackedfiles
-        set -g __fish_git_prompt_hide_untrackedfiles 1
+    if test "$fish_key_bindings" = "fish_vi_key_bindings"
+      or test "$fish_key_bindings" = "fish_hybrid_key_bindings"
+    switch $fish_bind_mode
+      case default
+        set prompt_text 'n '
+      case replace_one
+        set prompt_text 'r '
+      case visual
+        set prompt_text 'v '
+      #case insert
+      #  set prompt_text 'i '
+      case '*'
+        # Do nothing
     end
-
-    if not set -q __fish_git_prompt_color_branch
-        set -g __fish_git_prompt_color_branch magenta --bold
-    end
-    if not set -q __fish_git_prompt_showupstream
-        set -g __fish_git_prompt_showupstream "informative"
-    end
-    if not set -q __fish_git_prompt_char_upstream_ahead
-        set -g __fish_git_prompt_char_upstream_ahead "↑"
-    end
-    if not set -q __fish_git_prompt_char_upstream_behind
-        set -g __fish_git_prompt_char_upstream_behind "↓"
-    end
-    if not set -q __fish_git_prompt_char_upstream_prefix
-        set -g __fish_git_prompt_char_upstream_prefix ""
-    end
-
-    if not set -q __fish_git_prompt_char_stagedstate
-        set -g __fish_git_prompt_char_stagedstate "●"
-    end
-    if not set -q __fish_git_prompt_char_dirtystate
-        set -g __fish_git_prompt_char_dirtystate "✚"
-    end
-    if not set -q __fish_git_prompt_char_untrackedfiles
-        set -g __fish_git_prompt_char_untrackedfiles "…"
-    end
-    if not set -q __fish_git_prompt_char_conflictedstate
-        set -g __fish_git_prompt_char_conflictedstate "✖"
-    end
-    if not set -q __fish_git_prompt_char_cleanstate
-        set -g __fish_git_prompt_char_cleanstate "✔"
-    end
-
-    if not set -q __fish_git_prompt_color_dirtystate
-        set -g __fish_git_prompt_color_dirtystate blue
-    end
-    if not set -q __fish_git_prompt_color_stagedstate
-        set -g __fish_git_prompt_color_stagedstate yellow
-    end
-    if not set -q __fish_git_prompt_color_invalidstate
-        set -g __fish_git_prompt_color_invalidstate red
-    end
-    if not set -q __fish_git_prompt_color_untrackedfiles
-        set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
-    end
-    if not set -q __fish_git_prompt_color_cleanstate
-        set -g __fish_git_prompt_color_cleanstate green --bold
-    end
-
-    if not set -q __fish_prompt_normal
-        set -g __fish_prompt_normal (set_color normal)
-    end
-
-    set -l color_cwd
-    set -l prefix
-    set -l suffix
-    switch "$USER"
-        case root toor
-            if set -q fish_color_cwd_root
-                set color_cwd $fish_color_cwd_root
-            else
-                set color_cwd $fish_color_cwd
-            end
-            set suffix '#'
-        case '*'
-            set color_cwd $fish_color_cwd
-            set suffix '>'
-    end
-
-    # PWD
-    set_color $color_cwd
-    echo -n (prompt_pwd)
-    set_color normal
-
-    printf '%s ' (__fish_vcs_prompt)
-
-    if not test $last_status -eq 0
-        set_color $fish_color_error
-    end
-
-    echo -n "$suffix "
-
-    set_color normal
+  end
+  set_color $prompt_color
+  printf $prompt_text
+  set_color normal
 end
