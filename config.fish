12002f9d52e19fb63af33502f246c43937aaad3a
lasse <lasse-schloer@servermx.de>
2019-03-20 10:52:05 +0100

    always enable local echo in mosh

--- config.fish
+++ config.fish
@@ -5,6 +5,8 @@ end
 
 set -Ux EDITOR nvim
 
+set -Ux $MOSH_PREDICTION_DISPLAY always
+
 ## This is suggested by the kitty docs as a way to keep fish completions up-to-
 ## date. But it adds a substantial amount of startup time to the fish shell.
 #if type -q kitty
