8a9dff6f860307940aebc1b3a713bf871679a457
lasse <lasse-schloer@servermx.de>
2019-05-16 01:42:15 +0200

    added some conveniences

--- functions/aif-to-ogg.fish
+++ functions/aif-to-ogg.fish
@@ -1,7 +1,14 @@
 function aif-to-ogg --description "custom ad-hoc audio file conversion"
   switch (basename (pwd))
-  case 'aif'
+  case 'aif' 'wav'
+    if not test -d ../ogg
+      mkdir ../ogg
+    end
     for i in *.aif
+        avconv -i $i -c:a libvorbis -q:a 10\
+          ../ogg/(string split --max 1 --right "." $i)[1].ogg &
+    end
+    for i in *.wav
         avconv -i $i -c:a libvorbis -q:a 10\
           ../ogg/(string split --max 1 --right "." $i)[1].ogg &
     end
