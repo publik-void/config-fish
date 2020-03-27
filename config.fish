83ab9dba6277f1b4872db5dc378c8eaed40d07af
lasse <lasse-schloer@servermx.de>
2020-03-27 15:58:52 +0100

    added explicit PAGER variable and cleaned a couple of lines up

--- config.fish
+++ config.fish
@@ -4,16 +4,11 @@ case lasse-mbp-0 lasse-mba-0
 end
 
 set -Ux EDITOR nvim
-
+set -Ux PAGER less # Although I might want to consider using neovim?
 set -Ux MOSH_PREDICTION_DISPLAY always
-
-## This is suggested by the kitty docs as a way to keep fish completions up-to-
-## date. But it adds a substantial amount of startup time to the fish shell.
-#if type -q kitty
-#  kitty + complete setup fish | source
-#end
 
 set fish_escape_delay_ms 20
 
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+test -e {$HOME}/.iterm2_shell_integration.fish ;\
+and source {$HOME}/.iterm2_shell_integration.fish
 
