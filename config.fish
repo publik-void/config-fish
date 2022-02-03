880ead79f0c77c74561ebfdf7f0b3be118de77ea
lasse <lasse-schloer@servermx.de>
2022-02-03 16:19:17 +0100

    added a comment

--- config.fish
+++ config.fish
@@ -13,7 +13,7 @@ fish_vi_key_bindings
 set fish_cursor_default block
 set fish_cursor_insert line
 set fish_cursor_replace_one underscore
-set fish_vi_force_cursor
+set fish_vi_force_cursor # Note: Seems to be necessary, at least for iTerm2…
 
 test -e {$HOME}/.iterm2_shell_integration.fish ;\
 and source {$HOME}/.iterm2_shell_integration.fish
