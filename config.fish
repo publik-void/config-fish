b36db06b753cff18e4be92b10f88040d0bd31fde
lasse <lasse-schloer@servermx.de>
2022-02-03 16:17:42 +0100

    fixes

--- config.fish
+++ config.fish
@@ -10,6 +10,10 @@ set -Ux MOSH_PREDICTION_DISPLAY always
 set fish_escape_delay_ms 20
 
 fish_vi_key_bindings
+set fish_cursor_default block
+set fish_cursor_insert line
+set fish_cursor_replace_one underscore
+set fish_vi_force_cursor
 
 test -e {$HOME}/.iterm2_shell_integration.fish ;\
 and source {$HOME}/.iterm2_shell_integration.fish
--- functions/fish_vi_cursor.fish
+++
@@ -1,6 +0,0 @@
-function fish_vi_cursor --description "Change cursor shape depending on vi mode"
-  set fish_cursor_default block
-  set fish_cursor_insert line
-  set fish_cursor_replace_one underscore
-end
-
