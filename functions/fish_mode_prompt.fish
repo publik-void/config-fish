6855d0a832cc511ad18f76a76ae036b8e2a917ab
lasse <lasse-schloer@servermx.de>
2022-02-03 16:59:26 +0100

    fixes

--- functions/fish_mode_prompt.fish
+++ functions/fish_mode_prompt.fish
@@ -1,4 +1,42 @@
-function fish_mode_prompt --description "Display the mode for the prompt"
-  # Intentionally empty
+function fish_mode_prompt --description "Display the `vi` mode for the prompt"
+  set -l last_status $status
+
+  # Note: I specify colors directly here. I could alternatively use Fish's
+  # universal variables like `fish_color_error`, defined in
+  # `~/.config/fish/fish_variables`.
+  # Note: I'd like to use bold characters, but depending on the setup this may
+  # just result in a different color assignment, which seems to be the case with
+  # my setup (as of 2022-02-03), so yeah…
+  set -l prompt_color green
+  if not test $last_status -eq 0
+    set prompt_color brred
+  end
+
+  set_color $prompt_color
+
+  set -l prompt_text # Declare this as a local variable
+  switch $fish_bind_mode
+    case default
+      set prompt_text 'n'
+    case insert
+      switch $USER
+        case root toor
+          set prompt_text '#'
+        case lasse schloer pi tweek
+          set prompt_text '>'
+        case '*'
+          set prompt_text '$'
+      end
+    case replace_one
+      set prompt_text 'r'
+    case visual
+      set prompt_text 'v'
+    case '*'
+      set prompt_text '?'
+  end
+
+  printf $prompt_text
+
+  set_color normal
 end
 
--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,35 +1,4 @@
 function fish_prompt --description 'Write out the prompt'
-  set -l last_status $status
-  # Note: I specify colors directly here. I could alternatively use Fish's
-  # universal variables like `fish_color_error`, defined in
-  # `~/.config/fish/fish_variables`.
-  set -l prompt_color green
-  if not test $last_status -eq 0
-    set prompt_color brred
-  end
-
-  set -l prompt_text
-  switch "$USER"
-    case root toor
-      set prompt_text '# '
-    case '*'
-      set prompt_text '> '
-    end
-    if test "$fish_key_bindings" = "fish_vi_key_bindings"
-    switch $fish_bind_mode
-      case default
-        set prompt_text 'n '
-      case replace_one
-        set prompt_text 'r '
-      case visual
-        set prompt_text 'v '
-      #case insert
-      #  set prompt_text 'i '
-      case '*'
-        # Do nothing
-    end
-  end
-  set_color $prompt_color
-  printf $prompt_text
+  printf ' ' # '>' and so on gets output by `fish_mode_prompt.fish`.
   set_color normal
 end
