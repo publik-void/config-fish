09932404e27e582aa466234e035f6d5ba8986393
lasse <lasse-schloer@servermx.de>
2022-02-03 16:11:31 +0100

    added vi mode-edpendent cursor shape

---
+++ functions/fish_vi_cursor.fish
@@ -0,0 +1,6 @@
+function fish_vi_cursor --description "Change cursor shape depending on vi mode"
+  set fish_cursor_default block
+  set fish_cursor_insert line
+  set fish_cursor_replace_one underscore
+end
+
