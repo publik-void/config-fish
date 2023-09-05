b47a543866aa6cce23f59219986972de7ebc1f8c
Lasse Schlör <lasse-schloer@servermx.de>
2023-09-05 14:09:36 +0200

    list directories like symlinks depending on trailing slash

--- functions/l.fish
+++ functions/l.fish
@@ -1,6 +1,18 @@
-function l \
-    --wraps "ls -lhAF"
+function l --wraps=ls
   set opts -l -h -A -F
-  ls $opts $argv
+  set args
+  for arg in $argv
+    if string match --quiet -- "-*" "$arg"
+      set --append -- opts "$arg"
+    else
+      set --append -- args "$arg"
+    end
+  end
+  if [ (count $args) = 1 ]
+    if not string match --quiet -- "*/" "$args[1]"
+      set --append -- opts -d
+    end
+  end
+  ls $opts -- $args
 end
 
