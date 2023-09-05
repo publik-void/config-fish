f744d0cb627824576414265c188442a4309febda
Lasse Schlör <lasse-schloer@servermx.de>
2023-09-05 15:28:52 +0200

    improvements

--- functions/l.fish
+++ functions/l.fish
@@ -1,18 +1,26 @@
 function l --wraps=ls
-  set opts -l -h -A -F
-  set args
+  set -- opts -l -h -A -F
+  set -- args
+  set -- opts_end_reached false
+  if test -t 1
+    set --append -- opts --color=always
+  end
   for arg in $argv
-    if string match --quiet -- "-*" "$arg"
-      set --append -- opts "$arg"
+    if not $opts_end_reached && string match --quiet -- "-*" "$arg"
+      if [ "$arg" = "--" ]
+        set -- opts_end_reached true
+      else
+        set --append -- opts "$arg"
+      end
     else
       set --append -- args "$arg"
     end
   end
   if [ (count $args) = 1 ]
     if not string match --quiet -- "*/" "$args[1]"
-      set --append -- opts -d
+      set --prepend -- opts -d
     end
   end
-  ls $opts -- $args
+  ls $opts -- $args | string match --regex --invert -- "^total .*[^:]\$"
 end
 
