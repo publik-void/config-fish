29848d930949a2b0c0b3779092638367c2b94e43
Lasse Schlör <lasse-schloer@servermx.de>
2023-11-18 23:12:30 +0100

    added (non Neo-)Vim as a fallback `$EDITOR`

--- conf.d/environment.fish
+++ conf.d/environment.fish
@@ -1,8 +1,9 @@
 # Note: there are plans to phase out universal variables and the recommendation
 # is to only do `set --global` here
 
-type -q nvim; and set --global --export EDITOR nvim
-type -q less; and set --global --export PAGER less # Consider using neovim?
+type -q nvim && set --global --export EDITOR nvim
+type -q vim  && set --global --export EDITOR vim
+type -q less && set --global --export PAGER less # Consider using neovim?
 
 begin
   set --local paths "$HOME/bin" "$HOME/.juliaup/bin"
