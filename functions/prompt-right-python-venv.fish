e435e44f7ced8374cbe45ecfeabe5a22dd7462a7
lasse <lasse-schloer@servermx.de>
2024-07-02 14:55:33 +0200

    minor fix

--- functions/prompt-right-python-venv.fish
+++ functions/prompt-right-python-venv.fish
@@ -1,6 +1,6 @@
 function prompt-right-python-venv
   if set --query VIRTUAL_ENV
-    echo "$(set_color white)$VIRTUAL_ENV$(set_color normal) "
+    echo "$(set_color white)$VIRTUAL_ENV_PROMPT$(set_color normal) "
   end
 end
 
