b87eb10c231c3f2a8109374dab162b147cd016a1
Lasse Schlör <lasse-schloer@servermx.de>
2025-11-02 19:40:06 +0100

    added LuaRocks environment setup

--- conf.d/environment.fish
+++ conf.d/environment.fish
@@ -12,7 +12,8 @@ set --global --export PYENV_ROOT $HOME/.pyenv
 # environment-specific `python3`, unless forced with `fish -l`.
 if status is-login
   set --local paths "$HOME/bin" "$HOME/.local/bin" "$HOME/.juliaup/bin" \
-    "$PYENV_ROOT/bin" "$HOME/.cargo/bin" "$HOME/.ghcup/bin"
+    "$PYENV_ROOT/bin" "$HOME/.cargo/bin" "$HOME/.ghcup/bin" \
+    "$HOME/.luarocks/bin"
 
   if type -q fish_add_path
     fish_add_path --path $paths
@@ -21,3 +22,10 @@ if status is-login
   end
 end
 
+# LuaRocks needs host/user-specific environment variables. Alternatively, the
+# LuaRocks loader can be used in any Lua script. That's perhaps the way to go so
+# I'm commenting out the lines below.
+# if type -q luarocks
+#   # Unfortunately, `luarocks path` can take a second or two to run.
+#   set -q LUA_PATH; and set -q LUA_CPATH; or bass (luarocks path --no-bin)
+# end
