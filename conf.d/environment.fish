1e7db51e191ec6880ac6541e9f1c383e78457350
Lasse Schlör <lasse-schloer@servermx.de>
2025-11-27 21:24:49 +0100

    fixed Neovim priority for `EDITOR`

--- conf.d/environment.fish
+++ conf.d/environment.fish
@@ -1,8 +1,8 @@
 # Note: there are plans to phase out universal variables and the recommendation
 # is to only do `set --global` here
 
-command -q nvim && set --global --export EDITOR nvim
 command -q vim  && set --global --export EDITOR vim
+command -q nvim && set --global --export EDITOR nvim
 command -q less && set --global --export PAGER  less # Consider using neovim?
 
 set --global --export PYENV_ROOT $HOME/.pyenv
